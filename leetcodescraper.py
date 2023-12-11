34c8329deb1b231b1789fdf4b9708c0147175933
sanil18 <153499798+sanil18@users.noreply.github.com>
2023-12-11 16:25:54 +0545

    Add files via upload

---
+++ leetcodescraper.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+def scrape_leetcode_problems():
+    url = 'https://leetcode.com/problemset/all/'
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find the relevant elements containing problem information
+        problems = soup.find_all('div', class_='reactable-data-row')
+
+        data = []
+
+        for problem in problems:
+            title = problem.find('div', class_='question-title').text.strip()
+            difficulty = problem.find('span', class_='difficulty').text.strip()
+            tags = [tag.text.strip() for tag in problem.find_all('a', class_='reactable-data')]
+
+            # Get the problem statement
+            problem_url = f"https://leetcode.com{problem.find('a', class_='reactable-data').get('href')}"
+            problem_statement = get_problem_statement(problem_url)
+
+            data.append({'Title': title, 'Difficulty': difficulty, 'Tags': tags, 'Problem Statement': problem_statement})
+
+        save_to_csv(data)
+        print('Scraping and CSV creation completed successfully.')
+
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+
+def get_problem_statement(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find the element containing the problem statement
+        statement = soup.find('div', class_='content__u3I1 question-content__JfgR')
+
+        return statement.text.strip() if statement else 'Problem statement not available'
+
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+        return 'Problem statement not available'
+
+def save_to_csv(data):
+    try:
+        with open('leetcode_problems.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['Title', 'Difficulty', 'Tags', 'Problem Statement']
+            csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',')
+
+            csv_writer.writeheader()
+            csv_writer.writerows(data)
+
+    except IOError as e:
+        print(f"Error writing to CSV: {e}")
+
+        # Add print statements for debugging
+        print(response.text)  # Print the HTML content of the page
+        print(problems)  # Print the elements found during scraping
+
+
+# Call the function to initiate the scraping process
+scrape_leetcode_problems()
